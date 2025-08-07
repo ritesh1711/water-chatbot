4f7479950ec85516bc25846d1a78f4f27582395e
ritesh1711 <ritesh098765@gmail.com>
2025-08-07 14:23:53 +0530

    updated

---
+++ server.js
@@ -0,0 +1,14 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { chatController } from './controllers/chat.controller.js';
+
+const app = new Hono();
+
+// Enable CORS for frontend running at port 3001
+app.use('*', cors());
+
+app.get('/', (c) => c.text('Hello from Hono!'));
+app.post('/chat', chatController);
+
+export default app;
